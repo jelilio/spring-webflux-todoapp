7f0505e04b25e2d17cc4a7ae5421285f2c31ebb5
Jelili Adesina <jelilio.adesina@gmail.com>
2025-01-23 23:41:28 +0000

    configured user repository

---
+++ src/main/java/io/github/jelilio/todoapp/repository/UserRepository.java
@@ -0,0 +1,28 @@
+package io.github.jelilio.todoapp.repository;
+
+import io.github.jelilio.todoapp.entity.User;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import reactor.core.publisher.Mono;
+
+public interface UserRepository extends ReactiveCrudRepository<User, String> {
+  Mono<User> findByUsernameIgnoreCase(String username);
+
+  Mono<Long> countByUsernameIgnoreCase(String username);
+
+  @Query(value = "{'$and':[ {'email': ?0}, {'activatedDate': { '$ne': null } } ] }", count = true)
+  Mono<Long> countByEmailAvailable(String email);
+
+  @Query("{'$and':[ {'email': ?0}, {'activatedDate': null} ] }")
+  Mono<User> findByEmailAndNotActivated(String email);
+
+  @Query("{'$or':[ {'username': ?0}, {'email': ?0} ] }")
+  Mono<UserDetails> findByUsernameOrEmail(String usernameOrEmail);
+
+  @Query("{'$or':[ {'username': ?0}, {'email': ?0} ] }")
+  Mono<User> findUserByUsernameOrEmail(String usernameOrEmail);
+
+  @Query("{'$and':[ {'$or':[ {'username': ?0}, {'email': ?0} ] }, {'activatedDate': { '$ne': null } } ] }")
+  Mono<UserDetails> findByUsernameOrEmailAndActivated(String usernameOrEmail);
+}
