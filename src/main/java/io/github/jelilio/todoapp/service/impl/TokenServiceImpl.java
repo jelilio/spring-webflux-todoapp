03d57bae1d1aff72af5eb0b0cdd0e3891c91e5e2
Jelili Adesina <jelilio.adesina@gmail.com>
2025-01-25 11:38:09 +0000

    modified TokenService implementation

--- src/main/java/io/github/jelilio/todoapp/service/impl/TokenServiceImpl.java
+++ src/main/java/io/github/jelilio/todoapp/service/impl/TokenServiceImpl.java
@@ -19,7 +19,6 @@ import org.springframework.security.authentication.ReactiveAuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
