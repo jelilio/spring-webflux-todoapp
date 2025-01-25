e48271ed1a87cfa5d6cb1e0a6bf1238b1c4ee536
Jelili Adesina <jelilio.adesina@gmail.com>
2025-01-25 09:00:25 +0000

    resolved jwt cast to string exception

--- src/main/java/io/github/jelilio/todoapp/utils/SecurityUtil.java
+++ src/main/java/io/github/jelilio/todoapp/utils/SecurityUtil.java
@@ -1,12 +1,24 @@
 package io.github.jelilio.todoapp.utils;
 
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimAccessor;
 import reactor.core.publisher.Mono;
 
 public abstract class SecurityUtil {
   public static Mono<String> loggedInUsername() {
+    return loggedInCredentials()
+        .map(JwtClaimAccessor::getSubject);
+  }
+
+  public static Mono<Jwt> loggedInCredentials() {
     return ReactiveSecurityContextHolder.getContext()
         .map(context -> context.getAuthentication().getPrincipal())
-        .cast(String.class);
+        .cast(Jwt.class);
+  }
+
+  public static Mono<String> loggedInUserId() {
+    return loggedInCredentials()
+        .map(JwtClaimAccessor::getId);
   }
 }
